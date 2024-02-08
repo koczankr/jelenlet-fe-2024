1384d800cbf7431af83f676b8321221339639123
koczankr <koczankr@gmail.com>
2024-02-08 14:22:50 +0100

    alap MUI elemek domain nélkül + multi snackebar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-//import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
+import { Container, Box, CssBaseline, Button, TextField, Typography } from '@mui/material';
+import { SnackbarProvider, useSnackbar } from 'notistack';
 
-function App() {
+function SnackbarApp() {
+  const { enqueueSnackbar } = useSnackbar();
   const [currentTime, setCurrentTime] = useState('');
+  const [barcode, setBarcode] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBarcode = event.target.elements.barcode.value;
+
+    // Véletlenszerű stílus választása
+    const variants = ['success', 'warning', 'error'];
+    const randomVariant = variants[Math.floor(Math.random() * variants.length)];
+
+    enqueueSnackbar(newBarcode, { variant: randomVariant });
+    setBarcode(''); // Input mező ürítése
+  };
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -24,39 +33,52 @@ function App() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Typography component="h1" variant="h5" align="center">
-            Iskolai jelenlét-nyilvántartó alkalmazás
-          </Typography>
-          <p>{currentTime}</p>
-          <Box component="form"  noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Vonalkód"
-              name="barcode"
-              autoComplete="email"
-              autoFocus
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Elküld
-            </Button>
-          </Box>
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5" align="center">
+          Iskolai jelenlét-nyilvántartó alkalmazás
+        </Typography>
+        <p>{currentTime}</p>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="barcode"
+            label="Vonalkód"
+            name="barcode"
+            autoComplete="barcode"
+            autoFocus
+            value={barcode}
+            onChange={(e) => setBarcode(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Elküld
+          </Button>
         </Box>
+      </Box>
     </Container>
+  );
+}
+
+function App() {
+  return (
+    <SnackbarProvider maxSnack={5} anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}>
+      <SnackbarApp />
+    </SnackbarProvider>
 
   );
 }
